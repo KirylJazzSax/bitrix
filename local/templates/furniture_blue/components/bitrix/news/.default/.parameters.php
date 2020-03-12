c7dc8f0bcb245e03c5fc709d681f034645c12737
KirylJazzSax <matsak_92@mail.ru>
2020-03-12 17:31:31 +0300

    Удаляем компоненты из local

---
+++ local/templates/furniture_blue/components/bitrix/news/.default/.parameters.php
@@ -0,0 +1,12 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arTemplateParameters = array(
+    "SHOW_SPECIALDATE_META" => array(
+        "PARENT" => "ADDITIONAL_SETTINGS",
+        "NAME" => GetMessage("SHOW_META_SPECIALDATE"),
+        "TYPE" => "CHECKBOX",
+        "DEFAULT" => "N",
+    ),
+);
+?>
