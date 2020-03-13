2ea0850df890d6c71a9b5e17aeb52e852807505d
KirylJazzSax <matsak_92@mail.ru>
2020-03-13 17:07:56 +0300

    Редактируем старые файлы

--- local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.detail/.default/component_epilog.php
@@ -1,6 +1,5 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-\Bitrix\Main\Loader::includeModule('newsutils');
 $canonicalUtils = new \Local\NewsUtils\CanonicalIBlockUtils($arParams, $arResult);
 
 if ($canonicalUtils->isInParametersIdCanonical() && $nameElement = $canonicalUtils->getNameCanonicalElement()) {
