dc2fd374b10dea92535af8cf39fa46014582fea2
KirylJazzSax <matsak_92@mail.ru>
2020-03-13 17:06:53 +0300

    Удаляем ненужные файлы

--- local/modules/newsutils/include.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-\Bitrix\Main\Loader::registerAutoLoadClasses('newsutils', [
-    'Local\\NewsUtils\\NewsUtils' => 'lib/newsutils.php',
-    'Local\\NewsUtils\\CanonicalIBlockUtils' => 'lib/canonicaliblockutils.php'
-]);
