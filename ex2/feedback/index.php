2ffb782e1f1af188c09abadafe537f94742b9350
KirylJazzSax <matsak_92@mail.ru>
2020-03-16 17:29:36 +0300

    Создали раздел /ex2/feedback/

---
+++ ex2/feedback/index.php
@@ -0,0 +1,21 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Форма обратной связи");
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback", 
+	".default", 
+	array(
+		"USE_CAPTCHA" => "N",
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"EMAIL_TO" => "#EMAIL_TO#",
+		"REQUIRED_FIELDS" => array(
+			0 => "NAME",
+			1 => "EMAIL",
+			2 => "MESSAGE",
+		),
+		"EVENT_MESSAGE_ID" => array(
+		),
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ ex2/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Экзамен2");
+?><a href="ex2/feedback/">На форму обратной связи</a><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
