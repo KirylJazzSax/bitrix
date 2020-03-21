9e11b80a54d44a5bee7c5abecd1bdc2be91f500d
KirylJazzSax <matsak_92@mail.ru>
2020-03-21 19:08:25 +0300

    Настраиваем компонент

--- local/components/exam/simplecomp.exam/.parameters.php
+++ local/components/exam/simplecomp.exam/.parameters.php
@@ -2,11 +2,14 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 use Bitrix\Iblock\IblockTable;
+use Bitrix\Iblock\PropertyTable;
 
 \Bitrix\Main\Loader::includeModule('iblock');
 
-$catalogId = IblockTable::getList(['select' => ['ID'], 'filter' => ['IBLOCK_TYPE_ID' => 'products']])->fetch();
-$newsId = IblockTable::getList(['select' => ['ID'], 'filter' => ['IBLOCK_TYPE_ID' => 'news']])->fetch();
+$catalogId = IblockTable::getList(['select' => ['ID'], 'filter' => ['CODE' => 'furniture_products_s1']])->fetch()['ID'];
+$classifierId = IblockTable::getList(['select' => ['ID'], 'filter' => ['CODE' => 'manufacturing']])->fetch()['ID'];
+$detailUrlTemplate = IblockTable::getList(['select' => ['DETAIL_PAGE_URL'], 'filter' => ['ID' => $catalogId]])->fetch()['DETAIL_PAGE_URL'];
+$codeProp = PropertyTable::getByPrimary(12)->fetch()['CODE'];
 
 $arComponentParameters = array(
     "PARAMETERS" => array(
@@ -14,21 +17,33 @@ $arComponentParameters = array(
             "PARENT" => "BASE",
             "NAME" => "ID инфоблока с каталогом товаров",
             "TYPE" => "STRING",
-            "DEFAULT" => $catalogId['ID']
+            "DEFAULT" => $catalogId
         ),
-        "IBLOCK_NEWS_ID" => array(
+        "IBLOCK_MANUFACTURING_ID" => array(
             "PARENT" => "BASE",
-            "NAME" => "ID инфоблока с новостями",
+            "NAME" => "ID инфоблока с классификатором",
             "TYPE" => "STRING",
-            "DEFAULT" => $newsId['ID']
+            "DEFAULT" => $classifierId
         ),
-        "CODE_PROP_NEWS_LINK" => array(
-            "PARENT" => "BASE",
-            "NAME" => "Код пользовательского свойства разделов каталога, в котором хранится привязка к новостям",
+        "DETAIL_PAGE_URL" => array(
+            "PARENT" => "URL_TEMPLATES",
+            "NAME" => "Шаблон ссылки на детальный просмотр товара",
             "TYPE" => "STRING",
-            "DEFAULT" => "UF_NEWS_LINK"
+            "DEFAULT" => $detailUrlTemplate
+        ),
+        "CODE_PROP_FIRM" => array(
+            "PARENT" => "BASE",
+            "NAME" => "Код свойства товара, в котором хранится привязка товара к классификатору",
+            "TYPE" => "STRING",
+            "DEFAULT" => $codeProp,
         ),
         "CACHE_TIME"  =>  Array("DEFAULT"=>36000000),
+        "CACHE_GROUPS" => array(
+            "PARENT" => "CACHE_SETTINGS",
+            "NAME" => "Кеширование компонента должно ли зависеть от групп пользователей?",
+            "TYPE" => "CHECKBOX",
+            "DEFAULT" => "Y"
+        )
     )
 );
 ?>
