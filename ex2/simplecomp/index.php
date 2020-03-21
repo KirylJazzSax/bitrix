14d9c2dd2a9db3eb91536dbc6c7acf64c46863ee
KirylJazzSax <matsak_92@mail.ru>
2020-03-21 19:13:49 +0300

    Настройки компонента и его шаблон

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -1,13 +1,17 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 ?><?$APPLICATION->IncludeComponent(
-	"exam:simplecomp.exam",
-	"",
-	Array(
+	"exam:simplecomp.exam", 
+	".default", 
+	array(
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
-		"CODE_PROP_NEWS_LINK" => "UF_NEWS_LINK",
+		"CODE_PROP_FIRM" => "FIRM",
+		"DETAIL_PAGE_URL" => "#SITE_DIR#/products/#SECTION_ID#/#ID#/",
 		"IBLOCK_CATALOG_ID" => "2",
-		"IBLOCK_NEWS_ID" => "1"
-	)
+		"IBLOCK_MANUFACTURING_ID" => "7",
+		"COMPONENT_TEMPLATE" => ".default",
+		"CACHE_GROUPS" => "Y"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/components/exam/simplecomp.exam/templates/.default/template.php
+++ local/components/exam/simplecomp.exam/templates/.default/template.php
@@ -1,25 +1,24 @@
 <?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-use Local\Classes\Collections\Section\Section;
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+use Local\Classes\Collections\Product\Product;
 ?>
 <div>
     <h3>Каталог</h3>
     <?php foreach ($arResult['DATA'] as $item): ?>
         <ul>
             <li>
-                <strong><?= $item['news']->name ?></strong> - <?= $item['news']->date ?>
-                (<?= $item['section_names'] ?>)
+                <strong><?= $item['manufacturing']->name ?></strong>
                 <ul>
-                    <? /** @var $section Section */?>
-                    <?php foreach ($item['sections'] as $section): ?>
-                        <?php foreach ($section->products->getProducts() as $product): ?>
-                            <li>
+                    <? /** @var $product Product */ ?>
+                    <?php foreach ($item['products'] as $product): ?>
+                        <li>
+                            <a href="<?= $product->props->detailUrl ?>">
                                 <?= $product->name ?> -
-                                <?= $product->price ?> -
-                                <?= $product->material ?> -
-                                <?= $product->artNumber ?>
-                            </li>
-                        <?php endforeach; ?>
+                                <?= $product->props->price ?> -
+                                <?= $product->props->material ?> -
+                                <?= $product->props->artNumber ?>
+                            </a>
+                        </li>
                     <?php endforeach; ?>
                 </ul>
             </li>
