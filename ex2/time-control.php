eea92e39e59484d5c88d6335d23ed832ffa12aa4
KirylJazzSax <matsak_92@mail.ru>
2020-03-31 18:12:59 +0300

    Вывод тестирования производительности сайта

--- ex2/time-control.php
+++ ex2/time-control.php
@@ -2,6 +2,6 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности");
 ?>
-    <p>Самая ресурсоемкая страница: /services/index.php 30.80%, время 3.2073</p>
-    <p>Разница кеша с необходимыми ключами и работой компонента по умолчанию 4 247‬ КБ</p>
+    <p>Самая ресурсоемкая страница: /services/index.php 27.18%, время 3.3939</p>
+    <p>Компонент который больше всего загружался bitrix:menu его время 0.4942</p>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
