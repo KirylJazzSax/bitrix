b68b71ad9fa549bb80fcde0c7f49ae406e264b9a
KirylJazzSax <matsak_92@mail.ru>
2020-04-01 12:10:34 +0300

    Добавил шаблон ссылки без режима ЧПУ, и упростил шаблон компонента

--- local/components/complexcomp.exam-materials/templates/.default/sections_top.php
+++ local/components/complexcomp.exam-materials/templates/.default/sections_top.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -15,40 +15,37 @@ $this->setFrameMode(true);
 <?
 
 //ссылка на страницу станицу exampage
-if ($arParams['SEF_MODE'] === 'Y') {
-    $url = str_replace(['#PARAM1#', '#PARAM2#'], [123, 456], $arParams['SEF_URL_TEMPLATES']['exampage']);
-} else {
-    $url = $APPLICATION->GetCurPageParam('PARAM1=123&PARAM2=456');
-}
+$url = htmlspecialcharsbx($APPLICATION->GetCurPage())
+    . str_replace(['#PARAM1#', '#PARAM2#'], [123, 456], $arResult['URL_TEMPLATES']['exampage']);
 
-?><?=GetMessage("EXAM_TEXT_LINK_CP_PHOTO")?> <a href="<?=$url?>"><?=$url?></a>
+?><?= GetMessage("EXAM_TEXT_LINK_CP_PHOTO") ?> <a href="<?= $url ?>"><?= $url ?></a>
 
-<?$APPLICATION->IncludeComponent(
-	"bitrix:photo.sections.top",
-	"",
-	Array(
-		"IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
-		"IBLOCK_ID" => $arParams["IBLOCK_ID"],
-		"SECTION_COUNT" => $arParams["SECTION_COUNT"],
-		"ELEMENT_COUNT" => $arParams["TOP_ELEMENT_COUNT"],
-		"LINE_ELEMENT_COUNT" => $arParams["TOP_LINE_ELEMENT_COUNT"],
-		"SECTION_SORT_FIELD" => $arParams["SECTION_SORT_FIELD"],
-		"SECTION_SORT_ORDER" => $arParams["SECTION_SORT_ORDER"],
-		"ELEMENT_SORT_FIELD" => $arParams["TOP_ELEMENT_SORT_FIELD"],
-		"ELEMENT_SORT_ORDER" => $arParams["TOP_ELEMENT_SORT_ORDER"],
-		"FIELD_CODE" => $arParams["TOP_FIELD_CODE"],
-		"PROPERTY_CODE" => $arParams["TOP_PROPERTY_CODE"],
-		"DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
-		"SET_TITLE" => $arParams["SET_TITLE"],
-		"USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
-		"GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
-		"CACHE_TYPE" => $arParams["CACHE_TYPE"],
-		"CACHE_TIME" => $arParams["CACHE_TIME"],
-		"CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
+<? $APPLICATION->IncludeComponent(
+    "bitrix:photo.sections.top",
+    "",
+    Array(
+        "IBLOCK_TYPE" => $arParams["IBLOCK_TYPE"],
+        "IBLOCK_ID" => $arParams["IBLOCK_ID"],
+        "SECTION_COUNT" => $arParams["SECTION_COUNT"],
+        "ELEMENT_COUNT" => $arParams["TOP_ELEMENT_COUNT"],
+        "LINE_ELEMENT_COUNT" => $arParams["TOP_LINE_ELEMENT_COUNT"],
+        "SECTION_SORT_FIELD" => $arParams["SECTION_SORT_FIELD"],
+        "SECTION_SORT_ORDER" => $arParams["SECTION_SORT_ORDER"],
+        "ELEMENT_SORT_FIELD" => $arParams["TOP_ELEMENT_SORT_FIELD"],
+        "ELEMENT_SORT_ORDER" => $arParams["TOP_ELEMENT_SORT_ORDER"],
+        "FIELD_CODE" => $arParams["TOP_FIELD_CODE"],
+        "PROPERTY_CODE" => $arParams["TOP_PROPERTY_CODE"],
+        "DISPLAY_PANEL" => $arParams["DISPLAY_PANEL"],
+        "SET_TITLE" => $arParams["SET_TITLE"],
+        "USE_PERMISSIONS" => $arParams["USE_PERMISSIONS"],
+        "GROUP_PERMISSIONS" => $arParams["GROUP_PERMISSIONS"],
+        "CACHE_TYPE" => $arParams["CACHE_TYPE"],
+        "CACHE_TIME" => $arParams["CACHE_TIME"],
+        "CACHE_GROUPS" => $arParams["CACHE_GROUPS"],
 
-		"SECTION_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["section"],
-		"DETAIL_URL" => $arResult["FOLDER"].$arResult["URL_TEMPLATES"]["detail"],
-	),
-	$component
+        "SECTION_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["section"],
+        "DETAIL_URL" => $arResult["FOLDER"] . $arResult["URL_TEMPLATES"]["detail"],
+    ),
+    $component
 );
 ?>
