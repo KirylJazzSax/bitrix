92637ef041b6fddde9b05469167571bb93a694e3
KirylJazzSax <matsak_92@mail.ru>
2020-04-02 14:44:25 +0300

    Добавили сайт в ex2/site2 и добавили пункт в левом меню "второй сайт"

---
+++ ex2/site2/.top.menu.php
@@ -0,0 +1,19 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$aMenuLinks = Array(
+	Array(
+		"main",
+		"index.php",
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"about",
+		"about.php",
+		Array(), 
+		Array(), 
+		"" 
+	),
+);
+?>
